5722ff19b1d6a678aad61e7c2709816a8c9125f5
Esraa <Esogemy123@Gmail.com>
2026-08-16 22:56:59 +0300

    fix: set dynamic routes to RenderMode.Server

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,23 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'details/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'categories/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'subcategories/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
+    path: 'brandDetails/:id',
+    renderMode: RenderMode.Server,
+  },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
