06d91e08d87e7b0fbbcf53c6b628e6c16132f01c
Esraa <Esogemy123@Gmail.com>
2026-08-18 00:16:33 +0300

    fix: resolve SSR routing issues and cleanup

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,22 +2,6 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: 'details/:id',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'categories/:id',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'subcategories/:id',
-    renderMode: RenderMode.Server,
-  },
-  {
-    path: 'brandDetails/:id',
-    renderMode: RenderMode.Server,
-  },
-  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
